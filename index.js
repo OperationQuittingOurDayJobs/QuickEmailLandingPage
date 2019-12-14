1dbd0af045bfbfa6c1f6a284c796268a72e88b3b
Isaac Lee <isaacleewebdev@gmail.com>
2019-12-13 19:34:29 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,75 @@
+const bodyParser = require("body-parser");
+const nodemailer = require("nodemailer");
+const express = require("express");
+var escape = require("escape-html");
+
+const app = express();
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.SENDER_EMAIL_USERNAME,
+    pass: process.env.SENDER_EMAIL_PASSWORD
+  }
+});
+
+const mailOptions = {
+  from: process.env.SENDER_EMAIL_USERNAME,
+  to: process.env.RECIEVER_EMAIL_ADDRESS
+};
+
+const sendNewSubEmail = (email) =>
+  new Promise((resolve, reject) => {
+    transporter.sendMail(
+      {
+        ...mailOptions,
+        subject: "New KnuckleDragger Subscription",
+        text: email
+      },
+      (error, info) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(info);
+        }
+      }
+    );
+  });
+
+const sendErrorEmail = (email) =>
+  new Promise((resolve, reject) => {
+    transporter.sendMail(
+      {
+        ...mailOptions,
+        subject: "Error in KD Landing Page",
+        text: email
+      },
+      (error, info) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(info);
+        }
+      }
+    );
+  });
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.post("/", async (req, res) => {
+  try {
+    req.body.email = escape(req.body.email);
+    console.log("inc email:", req.body.email);
+    // send email
+    await sendNewSubEmail(req.body.email);
+    console.log("sent email");
+    res.redirect("/");
+  } catch (e) {
+    await sendErrorEmail(e);
+    res.sendStatus(400);
+  }
+});
+
+app.listen(process.env.PORT || 80, function() {
+  console.log("Listening on", this.address());
+});
